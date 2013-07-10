4e4f97290ab7339a2b16553cd5bdc8c458f439a5
toh2526 <toh2526@msn.com>
2013-07-10 19:12:06 +0700

    Enum class for customized message format

---
+++ lib/websocket_rails/enum.rb
@@ -0,0 +1,15 @@
+module WebsocketRails
+  class class Enum < Hash
+    def initialize(*members)
+      @rev = {}
+      members.each { |k,v| self[v] = k }
+    end
+    def [](k)
+       super || @rev[k]
+    end
+    def []=(k,v)
+      @rev[v] = k
+      super
+    end
+  end
+end
